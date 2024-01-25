fa5000fe5fe8fa932648c95721c5bc689725d4d1
Do Manh <manh.do@bnksolution.com>
2024-01-25 17:03:20 +0700

    create project

--- server.js
+++ server.js
@@ -9,27 +9,25 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 
-// // cho phép gọi api trên cùng laptop
-const cors = require('cors');
+const cors = require("cors");
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 // // cài ứng dụng sửu dụnng kiểu json từ db
 app.use(express.json());
 
-app.use(rootRouter)
+app.use(rootRouter);
 
 // cài static file
-const publicPathDirectory = path.join(__dirname, './public');
+const publicPathDirectory = path.join(__dirname, "./public");
 app.use("/public", express.static(publicPathDirectory));
 
-// lắng nghe sự kiện kết nối
 app.listen(port, async () => {
     try {
         await sequelize.authenticate();
